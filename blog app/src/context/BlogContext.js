ead459e01023a0e7d61767c58103f9eeb0d3ebc8
Shaun Birkmyre <shaunbirkmyre@gmail.com>
2020-04-14 17:12:17 +0100

    Section 12 In Progress: Added createDataContext to easily allow new context to be added to app. Blog posts can now be added and current list of blog posts will be rendered.

--- blog app/src/context/BlogContext.js
+++ blog app/src/context/BlogContext.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-  return (
-    <BlogContext.Provider
-      value={125}
-    >
-      {children}
-    </BlogContext.Provider>
-  );
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}`}];
+    default:
+      return state;
+  }
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
--- blog app/src/screens/IndexScreen.js
+++ blog app/src/screens/IndexScreen.js
@@ -1,18 +1,47 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <Button
+        title='Add Post'
+        onPress={addBlogPost}
+      />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name='trash-2' style={styles.icon} />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
