a9ea64037a8ed0d8253624cea9ee4fed08d9e7ce
Shaun Birkmyre <shaunbirkmyre@gmail.com>
2020-04-14 17:33:02 +0100

    Section 12 In Progress: Blog posts can be deleted. User can view a single blog post.

--- blog app/src/screens/IndexScreen.js
+++ blog app/src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
@@ -17,10 +17,18 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Feather name='trash-2' style={styles.icon} />
-            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Show', { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity
+                  onPress={() => deleteBlogPost(item.id)}
+                >
+                  <Feather name='trash-2' style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
