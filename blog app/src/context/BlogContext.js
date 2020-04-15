7f962f841577abce01603c81b1d3a6d92835b166
Shaun Birkmyre <shaunbirkmyre@gmail.com>
2020-04-15 23:24:13 +0100

    Section 12 In Progress: Create screen added.

--- blog app/src/context/BlogContext.js
+++ blog app/src/context/BlogContext.js
@@ -1,10 +1,17 @@
-import createDataContext from './createDataContext';
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}`}];
-    case 'delete_blogpost':
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+    case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
@@ -12,15 +19,20 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (title, content, callback) => {
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    callback();
   };
 };
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
-    dispatch({ type: 'delete_blogpost', payload: id });
+    dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost },
+  []
+);
--- blog app/src/screens/IndexScreen.js
+++ blog app/src/screens/IndexScreen.js
@@ -1,31 +1,33 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
-import { Context } from '../context/BlogContext';
-import { Feather } from '@expo/vector-icons';
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Button
-        title='Add Post'
-        onPress={addBlogPost}
-      />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
-              onPress={() => navigation.navigate('Show', { id: item.id })}
+              onPress={() => navigation.navigate("Show", { id: item.id })}
             >
               <View style={styles.row}>
-                <Text style={styles.title}>{item.title} - {item.id}</Text>
-                <TouchableOpacity
-                  onPress={() => deleteBlogPost(item.id)}
-                >
-                  <Feather name='trash-2' style={styles.icon} />
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather name="trash-2" style={styles.icon} />
                 </TouchableOpacity>
               </View>
             </TouchableOpacity>
@@ -36,20 +38,36 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        style={styles.headerIcon}
+        onPress={() => navigation.navigate("Create")}
+      >
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: 15,
-    borderTopWidth: 1,
-    borderColor: 'gray'
+    borderBottomWidth: 1,
+    borderColor: "gray",
   },
   title: {
-    fontSize: 18
+    fontSize: 18,
+  },
+  headerIcon: {
+    padding: 5,
   },
   icon: {
-    fontSize: 24
-  }
+    fontSize: 24,
+  },
 });
 
 export default IndexScreen;
